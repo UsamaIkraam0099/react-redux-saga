483b72cc569ce473a7b2523caae599441f6c8292
UsamaIkraam0099 <77922449+UsamaIkraam0099@users.noreply.github.com>
2021-05-17 16:42:19 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// FILES
+import './App.css';
+import Users from "./components/UserComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <Users />
+    </div>
+  );
+}
+
+export default App;
